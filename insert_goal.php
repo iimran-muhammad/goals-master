6448f4e78e162eab20d65a4283f211cba5c17c00
iimran-muhammad <70463516+iimran-muhammad@users.noreply.github.com>
2022-01-28 15:07:27 -0500

    upload initial files

---
+++ insert_goal.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'connect.php';
+
+$category = $_REQUEST['cat'];
+$text = $_REQUEST['text'];
+$date = $_REQUEST['goaldate'];
+$complete = $_REQUEST['complete'];
+
+if ($complete == '' || $complete == null){
+  $complete = 0;
+}
+/*
+$sql = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_complete) VALUES ";
+$sql .= "('" . $category . "',";
+$sql .= "'" . $text . "',";
+$sql .=  "'" . $date . "',";
+$sql .= "'" .$complete . "')";
+*/
+$sql = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_complete) VALUES
+	(
+	'$category',
+	'$text',
+	'$date',
+	$complete
+	)";
+
+//print $sql;
+if(mysqli_query($link, $sql)){
+  print ("Stored");
+} else {
+  print("Failed");
+}
+
+//echo "<script>location.href='index.php'</script>";
+header("location:index.php");  
+ ?>
